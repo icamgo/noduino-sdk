6caed75dd0181f892195a69c212f49aeaa66179f
Jack Tan <jiankemeng@gmail.com>
2018-04-22 14:59:35 +0800

    lib/noduino: update bmp180

--- lib/noduino/bmp085/bmp180.c
+++ lib/noduino/bmp085/bmp180.c
@@ -1,9 +1,9 @@
 /*
  *  Copyright (c) 2015 - 2025 MaiKe Labs
  *
- *  Library for BMP085 Digital pressure sensor 
+ *  Library for BMP180 Digital pressure sensor 
  *
- *  This library is ported from adafruit Arduino BMP085 project
+ *  This library is ported from adafruit Arduino BMP180 project
  *
  *	This program is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
@@ -27,9 +27,9 @@ uint16_t ac4, ac5, ac6;
 
 bool ICACHE_FLASH_ATTR bmp180_begin()
 {
-  uint8_t mode = BMP085_ULTRAHIGHRES;
-  if (mode > BMP085_ULTRAHIGHRES) 
-    mode = BMP085_ULTRAHIGHRES;
+  uint8_t mode = BMP180_ULTRAHIGHRES;
+  if (mode > BMP180_ULTRAHIGHRES) 
+    mode = BMP180_ULTRAHIGHRES;
   bmp180_oversampling = mode;
 
   wire_begin();
@@ -37,20 +37,20 @@ bool ICACHE_FLASH_ATTR bmp180_begin()
   if (bmp180_read8(0xD0) != 0x55) return false;
 
   /* bmp180_read calibration data */
-  ac1 = bmp180_read16(BMP085_CAL_AC1);
-  ac2 = bmp180_read16(BMP085_CAL_AC2);
-  ac3 = bmp180_read16(BMP085_CAL_AC3);
-  ac4 = bmp180_read16(BMP085_CAL_AC4);
-  ac5 = bmp180_read16(BMP085_CAL_AC5);
-  ac6 = bmp180_read16(BMP085_CAL_AC6);
+  ac1 = bmp180_read16(BMP180_CAL_AC1);
+  ac2 = bmp180_read16(BMP180_CAL_AC2);
+  ac3 = bmp180_read16(BMP180_CAL_AC3);
+  ac4 = bmp180_read16(BMP180_CAL_AC4);
+  ac5 = bmp180_read16(BMP180_CAL_AC5);
+  ac6 = bmp180_read16(BMP180_CAL_AC6);
 
-  b1 = bmp180_read16(BMP085_CAL_B1);
-  b2 = bmp180_read16(BMP085_CAL_B2);
+  b1 = bmp180_read16(BMP180_CAL_B1);
+  b2 = bmp180_read16(BMP180_CAL_B2);
 
-  mb = bmp180_read16(BMP085_CAL_MB);
-  mc = bmp180_read16(BMP085_CAL_MC);
-  md = bmp180_read16(BMP085_CAL_MD);
-#if (BMP085_DEBUG == 1)
+  mb = bmp180_read16(BMP180_CAL_MB);
+  mc = bmp180_read16(BMP180_CAL_MC);
+  md = bmp180_read16(BMP180_CAL_MD);
+#if (BMP180_DEBUG == 1)
   serial_printf("ac1 = %d\r\n",ac1);
   serial_printf("ac2 = %d\r\n",ac2);
   serial_printf("ac3 = %d\r\n",ac3);
@@ -78,44 +78,44 @@ int32_t ICACHE_FLASH_ATTR bmp180_computeB5(int32_t UT)
 
 uint16_t ICACHE_FLASH_ATTR bmp180_readRawTemperature()
 {
-  bmp180_write8(BMP085_CONTROL, BMP085_READTEMPCMD);
+  bmp180_write8(BMP180_CONTROL, BMP180_READTEMPCMD);
   delay(5);
-#if BMP085_DEBUG == 1
-  serial_printf("Raw temp: %d\r\n", bmp180_read16(BMP085_TEMPDATA));
+#if BMP180_DEBUG == 1
+  serial_printf("Raw temp: %d\r\n", bmp180_read16(BMP180_TEMPDATA));
 #endif
-  return bmp180_read16(BMP085_TEMPDATA);
+  return bmp180_read16(BMP180_TEMPDATA);
 }
 
 uint32_t ICACHE_FLASH_ATTR bmp180_readRawPressure()
 {
   uint32_t raw;
 
-  bmp180_write8(BMP085_CONTROL, BMP085_READPRESSURECMD + (bmp180_oversampling << 6));
+  bmp180_write8(BMP180_CONTROL, BMP180_READPRESSURECMD + (bmp180_oversampling << 6));
 
-  if (bmp180_oversampling == BMP085_ULTRALOWPOWER) 
+  if (bmp180_oversampling == BMP180_ULTRALOWPOWER) 
     delay(5);
-  else if (bmp180_oversampling == BMP085_STANDARD) 
+  else if (bmp180_oversampling == BMP180_STANDARD) 
     delay(8);
-  else if (bmp180_oversampling == BMP085_HIGHRES) 
+  else if (bmp180_oversampling == BMP180_HIGHRES) 
     delay(14);
   else 
     delay(26);
 
-  raw = bmp180_read16(BMP085_PRESSUREDATA);
+  raw = bmp180_read16(BMP180_PRESSUREDATA);
 
   raw <<= 8;
-  raw |= bmp180_read8(BMP085_PRESSUREDATA+2);
+  raw |= bmp180_read8(BMP180_PRESSUREDATA+2);
   raw >>= (8 - bmp180_oversampling);
 
  /* this pull broke stuff, look at it later?
   if (bmp180_oversampling==0) {
     raw <<= 8;
-    raw |= bmp180_read8(BMP085_PRESSUREDATA+2);
+    raw |= bmp180_read8(BMP180_PRESSUREDATA+2);
     raw >>= (8 - bmp180_oversampling);
   }
  */
 
-#if BMP085_DEBUG == 1
+#if BMP180_DEBUG == 1
   serial_printf("Raw pressure: %d\r\n", raw);
 #endif
   return raw;
@@ -130,7 +130,7 @@ int32_t ICACHE_FLASH_ATTR bmp180_readPressure()
   UT = bmp180_readRawTemperature();
   UP = bmp180_readRawPressure();
 
-#if BMP085_DEBUG == 1
+#if BMP180_DEBUG == 1
   // use datasheet numbers!
   UT = 27898;
   UP = 23843;
@@ -149,7 +149,7 @@ int32_t ICACHE_FLASH_ATTR bmp180_readPressure()
 
   B5 = bmp180_computeB5(UT);
 
-#if BMP085_DEBUG == 1
+#if BMP180_DEBUG == 1
   serial_printf("X1 = %d\r\n", X1);
   serial_printf("X2 = %d\r\n", X2);
   serial_printf("B5 = %d\r\n", B5);
@@ -162,7 +162,7 @@ int32_t ICACHE_FLASH_ATTR bmp180_readPressure()
   X3 = X1 + X2;
   B3 = ((((int32_t)ac1*4 + X3) << bmp180_oversampling) + 2) / 4;
 
-#if BMP085_DEBUG == 1
+#if BMP180_DEBUG == 1
   serial_printf("B6 = %d\r\n", B6);
   serial_printf("X1 = %d\r\n", X1);
   serial_printf("X2 = %d\r\n", X2);
@@ -175,7 +175,7 @@ int32_t ICACHE_FLASH_ATTR bmp180_readPressure()
   B4 = ((uint32_t)ac4 * (uint32_t)(X3 + 32768)) >> 15;
   B7 = ((uint32_t)UP - B3) * (uint32_t)( 50000UL >> bmp180_oversampling );
 
-#if BMP085_DEBUG == 1
+#if BMP180_DEBUG == 1
   serial_printf("X1 = %d\r\n", X1);
   serial_printf("X2 = %d\r\n", X2);
   serial_printf("B4 = %d\r\n", B4);
@@ -191,14 +191,14 @@ int32_t ICACHE_FLASH_ATTR bmp180_readPressure()
   X1 = (X1 * 3038) >> 16;
   X2 = (-7357 * p) >> 16;
 
-#if BMP085_DEBUG == 1
+#if BMP180_DEBUG == 1
   serial_printf("p = %d\r\n", p);
   serial_printf("X1 = %d\r\n", X1);
   serial_printf("X2 = %d\r\n", X2);
 #endif
 
   p = p + ((X1 + X2 + (int32_t)3791)>>4);
-#if BMP085_DEBUG == 1
+#if BMP180_DEBUG == 1
   serial_printf("p = %d\r\n", p); 
 #endif
   return p;
@@ -217,7 +217,7 @@ float ICACHE_FLASH_ATTR bmp180_readTemperature()
 
   UT = bmp180_readRawTemperature();
 
-#if BMP085_DEBUG == 1
+#if BMP180_DEBUG == 1
   serial_printf("readTemp: UT: %d\r\n", UT);
   // use datasheet numbers!
   UT = 27898;
@@ -248,12 +248,12 @@ uint8_t ICACHE_FLASH_ATTR bmp180_read8(uint8_t a)
 {
   uint8_t ret;
 
-  wire_beginTransmission(BMP085_I2CADDR);	// start transmission to device 
+  wire_beginTransmission(BMP180_I2CADDR);	// start transmission to device 
   wire_write(a);							// sends register address to read from
   wire_endTransmission();					// end transmission
   
-  wire_beginTransmission(BMP085_I2CADDR);	// start transmission to device 
-  wire_requestFrom(BMP085_I2CADDR, 1);		// send data n-bytes read
+  wire_beginTransmission(BMP180_I2CADDR);	// start transmission to device 
+  wire_requestFrom(BMP180_I2CADDR, 1);		// send data n-bytes read
   ret = wire_read();						// receive DATA
   wire_endTransmission();					// end transmission
 
@@ -264,12 +264,12 @@ uint16_t ICACHE_FLASH_ATTR bmp180_read16(uint8_t a)
 {
   uint16_t ret;
 
-  wire_beginTransmission(BMP085_I2CADDR);	// start transmission to device 
+  wire_beginTransmission(BMP180_I2CADDR);	// start transmission to device 
   wire_write(a);							// sends register address to read from
   wire_endTransmission();					// end transmission
   
-  wire_beginTransmission(BMP085_I2CADDR);	// start transmission to device 
-  wire_requestFrom(BMP085_I2CADDR, 2);		// send data n-bytes read
+  wire_beginTransmission(BMP180_I2CADDR);	// start transmission to device 
+  wire_requestFrom(BMP180_I2CADDR, 2);		// send data n-bytes read
   ret = wire_read();						// receive DATA
   ret <<= 8;
   ret |= wire_read();						// receive DATA
@@ -280,7 +280,7 @@ uint16_t ICACHE_FLASH_ATTR bmp180_read16(uint8_t a)
 
 void ICACHE_FLASH_ATTR bmp180_write8(uint8_t a, uint8_t d)
 {
-  wire_beginTransmission(BMP085_I2CADDR);	// start transmission to device 
+  wire_beginTransmission(BMP180_I2CADDR);	// start transmission to device 
   wire_write(a);							// sends register address to read from
   wire_write(d);							// write data
   wire_endTransmission();					// end transmission
--- lib/noduino/bmp085/bmp180.h
+++ lib/noduino/bmp085/bmp180.h
@@ -1,9 +1,9 @@
 /*
  *  Copyright (c) 2015 - 2025 MaiKe Labs
  *
- *  Library for BMP085 Digital pressure sensor 
+ *  Library for BMP180 Digital pressure sensor 
  *
- *  This library is ported from adafruit Arduino BMP085 project
+ *  This library is ported from adafruit Arduino BMP180 project
  *
  *	This program is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
@@ -20,36 +20,36 @@
  *
 */
 
-#ifndef __BMP085_H__
-#define __BMP085_H__
+#ifndef __BMP180_H__
+#define __BMP180_H__
 
 #include "noduino.h"
 
-#define BMP085_DEBUG	0
+#define BMP180_DEBUG	0
 
-#define BMP085_I2CADDR	0x77
+#define BMP180_I2CADDR	0x77
 
-#define BMP085_ULTRALOWPOWER 0
-#define BMP085_STANDARD      1
-#define BMP085_HIGHRES       2
-#define BMP085_ULTRAHIGHRES  3
-#define BMP085_CAL_AC1           0xAA  // R   Calibration data (16 bits)
-#define BMP085_CAL_AC2           0xAC  // R   Calibration data (16 bits)
-#define BMP085_CAL_AC3           0xAE  // R   Calibration data (16 bits)    
-#define BMP085_CAL_AC4           0xB0  // R   Calibration data (16 bits)
-#define BMP085_CAL_AC5           0xB2  // R   Calibration data (16 bits)
-#define BMP085_CAL_AC6           0xB4  // R   Calibration data (16 bits)
-#define BMP085_CAL_B1            0xB6  // R   Calibration data (16 bits)
-#define BMP085_CAL_B2            0xB8  // R   Calibration data (16 bits)
-#define BMP085_CAL_MB            0xBA  // R   Calibration data (16 bits)
-#define BMP085_CAL_MC            0xBC  // R   Calibration data (16 bits)
-#define BMP085_CAL_MD            0xBE  // R   Calibration data (16 bits)
+#define BMP180_ULTRALOWPOWER 0
+#define BMP180_STANDARD      1
+#define BMP180_HIGHRES       2
+#define BMP180_ULTRAHIGHRES  3
+#define BMP180_CAL_AC1           0xAA  // R   Calibration data (16 bits)
+#define BMP180_CAL_AC2           0xAC  // R   Calibration data (16 bits)
+#define BMP180_CAL_AC3           0xAE  // R   Calibration data (16 bits)    
+#define BMP180_CAL_AC4           0xB0  // R   Calibration data (16 bits)
+#define BMP180_CAL_AC5           0xB2  // R   Calibration data (16 bits)
+#define BMP180_CAL_AC6           0xB4  // R   Calibration data (16 bits)
+#define BMP180_CAL_B1            0xB6  // R   Calibration data (16 bits)
+#define BMP180_CAL_B2            0xB8  // R   Calibration data (16 bits)
+#define BMP180_CAL_MB            0xBA  // R   Calibration data (16 bits)
+#define BMP180_CAL_MC            0xBC  // R   Calibration data (16 bits)
+#define BMP180_CAL_MD            0xBE  // R   Calibration data (16 bits)
 
-#define BMP085_CONTROL           0xF4 
-#define BMP085_TEMPDATA          0xF6
-#define BMP085_PRESSUREDATA      0xF6
-#define BMP085_READTEMPCMD       0x2E
-#define BMP085_READPRESSURECMD   0x34
+#define BMP180_CONTROL           0xF4 
+#define BMP180_TEMPDATA          0xF6
+#define BMP180_PRESSUREDATA      0xF6
+#define BMP180_READTEMPCMD       0x2E
+#define BMP180_READPRESSURECMD   0x34
 
 
 bool bmp180_begin();  // by default go highres
