87f2800bb6f814ca2ec0948a9e5500994ab1c6f9
Jack Tan <jiankemeng@gmail.com>
2018-04-22 14:43:31 +0800

    examples/noduino: update the sst

--- examples/noduino/sst/main.c
+++ examples/noduino/sst/main.c
@@ -16,29 +16,111 @@
  *
 */
 #include "noduino.h"
+#include "sht2x.h"
+#include "tsl2561.h"
+#include "bmp085.h"
+
+irom void setup_tsl2561()
+{
+	if (!tsl2561_begin(TSL2561_ADDR_LOW)) {
+		serial_print("Could not found TSL2561 sensor!\r\n");
+	}
+
+	// You can change the gain on the fly, to adapt to brighter/dimmer
+	// light situations
+	//tsl2561_setGain(TSL2561_GAIN_0X);		// set no gain (for bright situtations)
+	tsl2561_setGain(TSL2561_GAIN_16X);		// set 16x gain (for dim situations)
+
+	// Changing the integration time gives you a longer time over which to sense light
+	// longer timelines are slower, but are good in very low light situtations!
+	tsl2561_setTiming(TSL2561_INTEGRATIONTIME_13MS);		// shortest integration time (bright light)
+	//tsl2561_setTiming(TSL2561_INTEGRATIONTIME_101MS);		// medium integration time (medium light)
+	//tsl2561_setTiming(TSL2561_INTEGRATIONTIME_402MS);		// longest integration time (dim light)
+}
+
+void do_tsl2561()
+{
+	// Simple data read example. Just read the infrared, fullspecrtrum diode
+	// or 'visible' (difference between the two) channels.
+	// This can take 13-402 milliseconds! Uncomment whichever of the following you want to read
+	uint16_t x = tsl2561_getLuminosity(TSL2561_VISIBLE);
+	//uint16_t x = tsl2561_getLuminosity(TSL2561_FULLSPECTRUM);
+	//uint16_t x = tsl2561_getLuminosity(TSL2561_INFRARED);
+
+	serial_printf("Luminosity:\t\t%d\r\n", x);
+
+	// More advanced data read example. Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
+	// That way you can do whatever math and comparisons you want!
+	uint32_t lum = tsl2561_getFullLuminosity();
+	uint16_t ir, full;
+	ir = lum >> 16;
+	full = lum & 0xFFFF;
+
+	serial_printf("IR:\t\t\t%d\r\n", ir);
+	serial_printf("Full:\t\t\t%d\r\n", full);
+	serial_printf("Visible:\t\t%d\r\n", full - ir);
+	serial_printf("Lux:\t\t\t%d\r\n", tsl2561_calculateLux(full, ir));
+}
+
+irom void setup_bmp180()
+{
+	if (!bmp085_begin()) {
+		serial_print("Could not find a valid BMP085 sensor!\r\n");
+	}
+}
+
+void do_bmp180()
+{
+	char obuf[16];
+
+	serial_printf("Pressure:\t\t%d Pa\r\n", bmp085_readPressure());
+
+	dtostrf(bmp085_readTemperature(), 16, 1, obuf);
+	serial_printf("Temperature:\t%s C\r\n", obuf);
+
+	// Calculate altitude assuming 'standard' barometric
+	// pressure of 1013.25 millibar = 101325 Pascal
+	serial_printf("Pressure at sealevel:\t%d Pa\r\n",
+			bmp085_readSealevelPressure(30));
+
+	// you can get a more precise measurement of altitude
+	// if you know the current sea level pressure which will
+	// vary with weather and such. If it is 1015 millibars
+	// that is equal to 101500 Pascals.
+	dtostrf(bmp085_readAltitude(102300), 16, 0, obuf);
+	serial_printf("Real altitude:\t%s M\r\n\r\n", obuf);
+}
+
+void do_sht2x()
+{
+	char t_buf[8];
+	char h_buf[8];
+
+	sht2x_reset();
+
+	dtostrf(sht2x_GetTemperature(), 5, 1, t_buf),
+	dtostrf(sht2x_GetHumidity(), 5, 1, h_buf);
+	serial_printf("Temperature(C):\t\t\t%s\r\n", t_buf);
+	serial_printf("Humidity(%):\t\t\t%s\r\n\r\n", h_buf);
+}
 
 void setup()
 {
-	pinMode(D4, OUTPUT);
-	pinMode(D5, OUTPUT);
-	pinMode(D6, OUTPUT);
-	digitalWrite(D4, LOW);
-	digitalWrite(D5, LOW);
-	digitalWrite(D6, LOW);
+	serial_begin(115200);
+	serial_printf("Start to test SHT2x sensor!\r\n");
+	wire_begin();
+
+	setup_tsl2561();
+	setup_bmp180();
 }
 
 void loop()
 {
-	digitalWrite(D4, HIGH);	// light on
-	delay(1000);				// delay 1000ms
-	digitalWrite(D4, LOW);		// light off
-	delay(1000);				// delay 1000ms
-	digitalWrite(D5, HIGH);	// light on
-	delay(2000);				// delay 1000ms
-	digitalWrite(D5, LOW);		// light off
-	delay(1000);				// delay 1000ms
-	digitalWrite(D6, HIGH);	// light on
-	delay(2000);				// delay 1000ms
-	digitalWrite(D6, LOW);		// light off
-	delay(1000);				// delay 1000ms
+	do_sht2x();
+	do_bmp180();
+	do_tsl2561();
+
+	serial_printf("#####################################\r\n");
+
+	delay(6000);
 }
