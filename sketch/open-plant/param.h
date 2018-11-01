f84152233969d8cb1de2b91bf8589d4b28d01106
Jack Tan <jiankemeng@gmail.com>
2018-11-01 22:37:43 +0800

    sketch/open-plant: support the parameter sector

---
+++ sketch/open-plant/param.h
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (c) 2016 - 2025 MaiKe Labs
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+#ifndef __PARAM_H__
+#define __PARAM_H__
+
+/* 
+ * 1MB spi flash: 0x0FA000
+ * 4MB spi flash: 0x3FA000
+ */
+#define PARAM_START_SEC		0xFA
+
+struct dev_param {
+	float temp;
+	float humi;
+	int light;
+	float co2;
+	uint8_t realtime;				/* 1: mqtt enable, 0: mqtt disable */
+	uint8_t airkiss_nff_on;
+	uint8_t pack[3];
+} __attribute__((aligned(4), packed));
+
+uint8_t param_get_realtime();
+void param_set_realtime(uint8_t d);
+void param_save();
+void param_erase_all();
+void param_init();
+
+#endif
