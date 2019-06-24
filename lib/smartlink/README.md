e870cfb4c19d05224851658ba8d6efcfd534748c
Jack Tan <jiankemeng@gmail.com>
2019-06-25 00:01:15 +0800

    lib/smartlink: add the free version of smarklink
    upstream is: github.com/ly0/esp8266-smartlink

---
+++ lib/smartlink/README.md
@@ -0,0 +1,70 @@
+﻿esp8266-smartlink 
+=======
+
+Instruction in English is followed by Chinese.
+
+### 名词定义
+*Smartlink* 用于在物理网设备没有外部输入端时，使用手机对其进行配置的一种通用的说法。  
+
+### Definition of Smartlink
+*Smartlink* is a generic phrase when you configure your IoT devices by mobile phone, PC etc. instead of physical external devices.
+
+### 使用方法
+在需要使用 `smartlink` 的地方调用
+
+```c
+smartlink_init(callback_func, callback_func_args, callback_failed_func, max_retry);
+```
+
+**手机端**: 设置完成后会调用 `(*callback_func)(callback_func_args)`，连接失败则会调用 `(*callback_failed_func)(callback_func_args)`
+使用手机手动添加一个Wi-Fi网络，ssid为 ssid<!-SL-!>password,确定的几乎一瞬间 8266 就会收到并且开始配置。
+
+**SDK 版本**: 0.9.4 by Espressif
+
+### Usage
+Call `smartlink_init(callback_func, callback_func_args, callback_failed_func, max_retry);` when you want to use Smarklink to configure.
+
+`callback_func`: callback function when connect target successfully.  
+`callback_func_args`: arguments of success callback function.  
+`callback_failed_func`: callback function when fail to connect target.  
+`max_retry`: maximiun number of retry.
+
+**Mobile**: Add a ssid named `ssid<!-SL-!>password` manually, 8266 will receive this request almost immediately and finish configuration.
+
+**SDK Version**: 0.9.4 by Espressif
+
+### 原理
+`Probe Request` 包的 `tagged parameter` 字段是不加密的，这个字段中的 `ssid` 可以每次最大传送 32 字节的数据。
+
+### Principle
+`tagged parameter` in `Probe Request` packet is not encrypted, we can reuse this area to send data to our IoT devices.
+
+### 协议
+The MIT License (MIT)
+
+Copyright (c) 2014 latyas
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+
+### License
+See above.
+
+### Reference
+[Cisco Support Forums](https://supportforums.cisco.com/document/52391/80211-frames-starter-guide-learn-wireless-sniffer-traces)
---
+++ lib/smartlink/smartlink.c
@@ -0,0 +1,132 @@
+#include "c_types.h"
+#include "osapi.h"
+#include "network_80211.h"
+#include "user_interface.h"
+
+#define SMARTLK_TOKEN "<!-SL-!>"
+#define SMARTLK_TOKEN_LEN (strlen(SMARTLK_TOKEN))
+
+void smartlink_wifi_promiscuous_rx(uint8_t*, uint16);
+void smartlink_received(char*, char*);
+
+void (*smartlink_received_cb)(void*) = NULL;
+void *smartlink_received_cb_args = NULL;
+void (*smartlink_failed_cb)() = NULL;
+
+uint8_t max_wait = 5;
+
+void ICACHE_FLASH_ATTR
+smartlink_init(void* cb, void* args, void* fail, uint8_t max)
+{	
+	os_printf("\n\n");
+	os_printf("[*] reset wifi config \n");
+	{
+		wifi_station_disconnect();
+		wifi_station_set_config(NULL);
+	}
+
+	os_printf("[*] set opmode=0x01\n");
+    wifi_set_opmode(0x01);
+    
+    os_printf("[*] set smartlink_received callback function\n");
+    smartlink_received_cb = cb;
+    smartlink_received_cb_args = args;
+    smartlink_failed_cb = fail;
+    max_wait = max;
+
+    os_printf("[*] enable wifi promiscuous\n");
+    wifi_promiscuous_enable(1);
+    wifi_set_promiscuous_rx_cb(smartlink_wifi_promiscuous_rx);
+}
+
+void ICACHE_FLASH_ATTR
+smartlink_wifi_promiscuous_rx(uint8_t *buf, uint16 len)
+{
+
+    uint16 i;
+    uint8_t type;
+    
+   // os_printf("smartlink_wifi_promiscuous_rx -----\n");
+    lpframectrl_80211 framectrl;
+    struct router_info *info = NULL;
+    if (len < 64) {
+        return;
+    }
+    struct sniffer_buf *sniffer = (struct sniffer_buf*)buf;
+    buf +=sizeof(struct RxControl);
+    struct probe_request_80211 *probe_buf = (struct probe_request_80211*)buf;
+	if (FRAME_TYPE_MANAGEMENT == probe_buf->framectrl.Type) {
+    	/* Management frame */
+    	if (FRAME_SUBTYPE_PROBE_REQUEST == probe_buf->framectrl.Subtype) {
+          
+    		/* Probe Request */
+    		ptagged_parameter tag = (ptagged_parameter)(buf + sizeof(probe_request));
+    		if (tag->tag_length != 0)
+    		{
+	    		uint8_t ssid_buff[32];
+	    		os_memset(ssid_buff, 0, 32);
+	    		os_memcpy(ssid_buff, (uint8_t *)tag + 2, tag->tag_length);
+	    		char *pos = (char *)os_strstr(ssid_buff, SMARTLK_TOKEN);
+	    		if (pos != NULL)
+	    		{
+	    			uint8_t ssid[40];
+	    			uint8_t password[40];
+	    			os_memset(ssid, 0, 32);
+	    			os_memset(password, 0, 32);
+	    			/* ssid */
+	    			os_memcpy(ssid, ssid_buff, pos - (char *)ssid_buff);
+	    			/* password */
+	    			os_memcpy(password, pos + SMARTLK_TOKEN_LEN, tag->tag_length - SMARTLK_TOKEN_LEN - strlen(ssid));
+	    			smartlink_received(ssid, password);
+	    		}
+    		}
+    	}
+	}
+}
+
+void ICACHE_FLASH_ATTR
+smartlink_received(char* ssid, char* password)
+{
+	os_printf("[*] received SSID:%s PASSWORD:%s\n", ssid, password);
+	//wifi_promiscuous_enable(0);
+	os_printf("[*] change mode: STATION \n");
+	wifi_set_opmode(0x01);
+	{
+		struct station_config config;
+		os_strcpy(config.ssid, ssid);
+		os_strcpy(config.password, password);
+		wifi_station_set_config(&config);
+	}
+    system_restart();
+#if 0
+	os_printf("[*] connecting to %s ... \n", ssid);
+	wifi_station_connect();
+
+	os_printf("[*] check connection to %s ... \n", ssid);
+	{
+		uint8_t status;
+		uint8_t retry;
+		for(retry = 0; retry < max_wait; ++retry)
+		{
+			status = wifi_station_get_connect_status();
+			if (STATION_IDLE == status || STATION_GOT_IP == status)
+			{	
+				os_printf("[*] connected to %s", ssid);
+				if (smartlink_received_cb)
+				{
+					(*smartlink_received_cb)(smartlink_received_cb_args);
+				}
+			} else {
+				os_printf("[*] probing AP:%s\n", ssid);
+				//vTaskDelay(1000 / portTICK_RATE_MS);			
+			}
+		}	
+		
+	}
+
+	if (smartlink_failed_cb)
+	{
+		(*smartlink_failed_cb)();
+	}	
+#endif
+}
