9be202cd2d0524e70126a849db04ec129e3fe10e
Jack Tan <jiankemeng@gmail.com>
2019-11-15 13:52:05 +0800

    efm32: add the example of internal temp of the adc

---
+++ examples/efm32/intertemp/main.cpp
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (c) 2019 - 2029 MaiKe Labs
+ *
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+#include "Arduino.h"
+
+void setup()
+{
+	Serial.setRouteLoc(1);
+	Serial.begin(115200);
+}
+
+/*
+ * Output:
+ *
+ *  Read internal temperature :26.59 'C
+ *
+*/
+void loop()
+{
+	Serial.print("Read internal temperature :");
+
+	float temp = adc.temperatureCelsius();
+	Serial.print(temp);
+	Serial.println(" 'C");
+
+	delay(5000);
+
+	//EMU_EnterEM2(true);
+}
