ec22ba759df68ae4e5e366b59b1439a7c2352a77
Jack Tan <jiankemeng@gmail.com>
2019-11-28 07:46:26 +0800

    examples/efm32: update the float16

--- examples/efm32/float16/main.cpp
+++ examples/efm32/float16/main.cpp
@@ -29,7 +29,7 @@ void test_fp16()
 
 	float2halfp(&myfp16, &myFloat, 1);		// convert 1 float to fp16
 
-	Serial.println("Testing the float16 function... ");
+	Serial.println("Testing the general function of float16... ");
 
 	Serial.print("Convert fp32=1.245 to fp16, HEX: ");
 	Serial.println(myfp16, HEX);
@@ -39,6 +39,19 @@ void test_fp16()
 
 	Serial.print("fp16 to float: ");
 	Serial.println((double)myfp32, 3);
+}
+
+void hex2fp16(uint16_t h)
+{
+	char omsg[32];
+
+	float x = 0;
+	halfp2float(&x, &h, 1);
+
+	sprintf(omsg, "0x%04X = %f\r\n", h, x);
+	Serial.print(omsg);
+
+	//printf("0x%04X = %f\r\n", h, x);
 }
 
 void setup()
@@ -62,6 +75,37 @@ void loop()
 
 	// test the function
 	test_fp16();
+
+	hex2fp16(0x0000);		// 0
+	hex2fp16(0x8000);		// -0
+	hex2fp16(0xC000);		// -2
+
+	hex2fp16(0x3BFF);		// 0.99951
+	hex2fp16(0x3C00);		// 1
+	hex2fp16(0x3C01);		// 1.001
+	hex2fp16(0x3C01);		// 1.001
+	hex2fp16(0x3C02);		// 1.001
+	hex2fp16(0x3C03);		// 1.001
+	hex2fp16(0x3C04);		// 1.001
+	hex2fp16(0x3CFB);		// 1.001
+	hex2fp16(0x3CFF);		// 1.001
+
+	hex2fp16(0xBBFF);
+	hex2fp16(0xBC00);
+	hex2fp16(0xBC01);
+	hex2fp16(0xBC02);
+	hex2fp16(0xBC03);
+	hex2fp16(0xBC04);
+	hex2fp16(0xBCFB);
+	hex2fp16(0xBCFF);
+
+	hex2fp16(0x3555);		// 0.333251953125
+
+	hex2fp16(0x0400);		// 0.000061035
+	hex2fp16(0x03ff);		// 0.000060976
+	hex2fp16(0x0001);		// 0.000000059605
+
+	hex2fp16(0x7bff);		// 65504
 
 	delay(5000);
 }
