768dc98ab4bacfc25595ed36c9f015318ff7fc4f
Jack Tan <jiankemeng@gmail.com>
2019-11-28 10:33:27 +0800

    examples/efm32: update the float16

--- examples/efm32/float16/main.cpp
+++ examples/efm32/float16/main.cpp
@@ -23,7 +23,7 @@
 void test_fp16()
 {
 
-	float myFloat = 1.245;
+	float myFloat = 13.245;
 
 	uint16_t myfp16 = 0;
 
@@ -31,7 +31,7 @@ void test_fp16()
 
 	Serial.println("Testing the general function of float16... ");
 
-	Serial.print("Convert fp32=1.245 to fp16, HEX: ");
+	Serial.print("Convert fp32=13.245 to fp16, HEX: ");
 	Serial.println(myfp16, HEX);
 	 
 	float myfp32 = 0;
@@ -39,6 +39,8 @@ void test_fp16()
 
 	Serial.print("fp16 to float: ");
 	Serial.println((double)myfp32, 3);
+
+	Serial.println("---------------------------------------------------");
 }
 
 void hex2fp16(uint16_t h)
@@ -48,10 +50,22 @@ void hex2fp16(uint16_t h)
 	float x = 0;
 	halfp2float(&x, &h, 1);
 
-	sprintf(omsg, "0x%04X = %f\r\n", h, x);
-	Serial.print(omsg);
+	Serial.print("0x");
+	Serial.print(h, HEX);
+	Serial.print(" = ");
+	Serial.println((double)x, 10);
+}
 
-	//printf("0x%04X = %f\r\n", h, x);
+
+void float2fp16hex(float f32)
+{
+	uint16_t f = 0;
+
+	float2halfp(&f, &f32, 1);
+
+	Serial.print((double)f32, 10);
+	Serial.print(" = 0x");
+	Serial.println((uint16_t)f, HEX);
 }
 
 void setup()
@@ -65,6 +79,7 @@ void loop()
 {
 	__fp16 f1 = 0.232;
 	
+	Serial.println("---------------------------------------------------");
 	Serial.println("Testing the float16, supported by gcc... ");
 
 	Serial.print("sizeof(fp16): ");
@@ -72,40 +87,114 @@ void loop()
 
 	Serial.print("f1 = ");
 	Serial.println((double)f1, 4);
+	Serial.println("---------------------------------------------------");
 
 	// test the function
 	test_fp16();
 
-	hex2fp16(0x0000);		// 0
-	hex2fp16(0x8000);		// -0
-	hex2fp16(0xC000);		// -2
+	Serial.println("----- Test the [0, 1] -----");
+	// 0x0 = 0
+	// 0x3c00 = 1.0
+	float2fp16hex(0);
 
-	hex2fp16(0x3BFF);		// 0.99951
-	hex2fp16(0x3C00);		// 1
-	hex2fp16(0x3C01);		// 1.001
-	hex2fp16(0x3C01);		// 1.001
-	hex2fp16(0x3C02);		// 1.001
-	hex2fp16(0x3C03);		// 1.001
-	hex2fp16(0x3C04);		// 1.001
-	hex2fp16(0x3CFB);		// 1.001
-	hex2fp16(0x3CFF);		// 1.001
+	hex2fp16(0x2);
+	hex2fp16(0x3);
+	hex2fp16(0x4);
 
-	hex2fp16(0xBBFF);
-	hex2fp16(0xBC00);
-	hex2fp16(0xBC01);
-	hex2fp16(0xBC02);
-	hex2fp16(0xBC03);
-	hex2fp16(0xBC04);
-	hex2fp16(0xBCFB);
-	hex2fp16(0xBCFF);
+	hex2fp16(0x0080);
+	hex2fp16(0x0800);
 
-	hex2fp16(0x3555);		// 0.333251953125
+	hex2fp16(0x2000);
+	hex2fp16(0x2001);
+	hex2fp16(0x2002);
+	hex2fp16(0x3000);
 
-	hex2fp16(0x0400);		// 0.000061035
-	hex2fp16(0x03ff);		// 0.000060976
-	hex2fp16(0x0001);		// 0.000000059605
+	hex2fp16(0x3bfd);
+	hex2fp16(0x3bfe);
+	hex2fp16(0x3bff);
 
+	float2fp16hex(1);
+
+	Serial.println("----- Test the [-1, 0] -----");
+	hex2fp16(0x8002);
+	hex2fp16(0x8003);
+	hex2fp16(0x8004);
+
+	hex2fp16(0x8080);
+	hex2fp16(0x8800);
+
+	hex2fp16(0xA000);
+	hex2fp16(0xA001);
+	hex2fp16(0xA002);
+
+	hex2fp16(0xB000);
+
+	hex2fp16(0xbbfd);
+	hex2fp16(0xbbfe);
+	hex2fp16(0xbbff);
+
+
+	Serial.println("----- Test the [1, 2048] -----");
+	// 0x3c00 = 1, 0x6800 = 2048
+	hex2fp16(0x3c01);
+	hex2fp16(0x3c02);
+
+	hex2fp16(0x3CFD);		// 1.247
+	hex2fp16(0x3CFE);		// 1.248
+	hex2fp16(0x3CFF);		// 1.249
+
+	float2fp16hex(3.676);
+	float2fp16hex(3.677);
+	float2fp16hex(3.678);
+
+	float2fp16hex(3.544);
+	float2fp16hex(3.545);
+	float2fp16hex(3.546);
+
+	hex2fp16(0x3d00);
+	hex2fp16(0x3e00);
+
+	hex2fp16(0x4000);
+	hex2fp16(0x4001);
+	hex2fp16(0x4002);
+
+	hex2fp16(0x5000);
+	hex2fp16(0x5001);
+	hex2fp16(0x5002);
+	float2fp16hex(32.1234);
+	float2fp16hex(32.2234);
+	float2fp16hex(32.2254);
+
+	hex2fp16(0x6000);
+	hex2fp16(0x6001);
+	hex2fp16(0x6002);
+
+	hex2fp16(0x67fe);
+	hex2fp16(0x67ff);
+
+	float2fp16hex(2048);
+
+	Serial.println("----- Test the [2048, NaN] -----");
+	// 0x6800 = 2048, 0x6c00 = 4096
+	float2fp16hex(4096);
+
+	float2fp16hex(8192);
+
+	float2fp16hex(16384);
+
+	float2fp16hex(32768);
+
+	float2fp16hex(65504);
+
+	hex2fp16(0x7bfd);		// 65440
+	hex2fp16(0x7bfe);		// 65472
 	hex2fp16(0x7bff);		// 65504
+	hex2fp16(0x7c00);
+	hex2fp16(0x7c01);
+	hex2fp16(0x7d00);
+	hex2fp16(0x7e00);
+	hex2fp16(0x7f00);
+	hex2fp16(0x8000);
 
 	delay(5000);
 }
