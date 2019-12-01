26de7f5456583b70d3560f3a05ad58e85bcba94d
Jack Tan <jiankemeng@gmail.com>
2019-12-01 22:58:57 +0800

    examples/efm32/aes: add the decrypto feature

--- examples/efm32/aes/main.cpp
+++ examples/efm32/aes/main.cpp
@@ -25,13 +25,9 @@
  * mode with a 128 bits key and initial vector (iv) of 16 bytes
  *
 */
-const uint8_t msg[64] __attribute__((aligned(4))) = {0x47, 0x4F, 0x33, 0x00, 0x00, 0x00, 0x02, 0xC5,
+const uint8_t msg[32] __attribute__((aligned(4))) = {0x47, 0x4F, 0x33, 0x00, 0x00, 0x00, 0x02, 0xC5,
                             0x6E, 0x3B, 0xBA, 0x00, 0x8F, 0x0E, 0x29, 0x02,
                             0xE5, 0x75, 0x05, 0x15, 0x00, 0x41, 0x4D, 0x8C,
-                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 const uint8_t key[16] __attribute__((aligned(4))) = {0x64, 0x63, 0x47, 0x4F, 0x64, 0x63, 0x47, 0x4F,
@@ -40,7 +36,9 @@ const uint8_t key[16] __attribute__((aligned(4))) = {0x64, 0x63, 0x47, 0x4F, 0x6
 const uint8_t iv[16] __attribute__((aligned(4))) = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
                      	0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
 
-uint8_t e_msg[64]; /* Output buffer for encrypted data (ciphertext). */
+uint8_t e_msg[32] __attribute__((aligned(4))); /* Output buffer for encrypted data (ciphertext). */
+
+uint8_t d_key[16] __attribute__((aligned(4)));
 
 void setup()
 {
@@ -52,7 +50,7 @@ void setup()
 
 void print_msg(uint8_t *pkt)
 {
-	for (int i = 0; i < 63; i++) {
+	for (int i = 0; i < 32; i++) {
 
 		if (pkt[i] < 16)
 			Serial.print("0");
@@ -73,7 +71,7 @@ void loop()
 
 	start = millis();
 
-	AES_CBC128(e_msg, msg, 64, key, iv, true);  /* true means encrypt. */
+	AES_CBC128(e_msg, msg, 32, key, iv, true);  /* true means encrypt. */
 
 	end = millis();
 
@@ -82,7 +80,12 @@ void loop()
 	Serial.println("ms");
 
 	Serial.println("The Encrypyted MSG:");
+	print_msg(e_msg);
 
+	AES_DecryptKey128(d_key, key);
+
+	AES_CBC128(e_msg, e_msg, 32, d_key, iv, false);
+	Serial.println("The Decrypyted MSG:");
 	print_msg(e_msg);
 
 	delay(6000);
