036e4da8f70590626ca638121ea55f43a24688fa
Jack Tan <jiankemeng@gmail.com>
2019-12-02 18:56:52 +0800

    arch/avr/libs: update u8g2
    add the app x1 icon

--- arch/avr/libs/U8g2/src/clib/u8g2_fonts.c
+++ arch/avr/libs/U8g2/src/clib/u8g2_fonts.c
@@ -57,6 +57,19 @@ const uint8_t u8g2_font_open_iconic_embedded_1x_t[246] U8G2_FONT_SECTION("u8g2_f
   "!\0N\20\207$^\70\30\311\24\222J#\261\330\10\0O\20\210$\26-\70\244\3\342\200\70$\16"
   "!\1P\14\207$N%\32\7\315Q\225\0\0\0\0\4\377\377\0";
 /*
+  Fontname: open_iconic_app_1x
+  Copyright: https://github.com/iconic/open-iconic, SIL OPEN FONT LICENSE
+  Glyphs: 9/9
+  BBX Build Mode: 0
+*/
+const uint8_t u8g2_font_open_iconic_app_1x_t[164] U8G2_FONT_SECTION("u8g2_font_open_iconic_app_1x_t") = 
+  "\11\0\3\3\4\4\1\3\5\10\10\0\0\10\0\10\0\0\21\0\0\0\207@\21\210\211\67\213\214fD"
+  "\261$\16\220\314\42\42\0A\16\207\211q\210J\17\222\24\213$\345\20B\16\207\211\361\16\70DR\16"
+  "\222\310a\0C\17\210\211)\245\34$\242\212\204\42)\35\6D\12h\215Q\23K.\265\6E\17\210"
+  "\211E\13F\302j!q$\30#\1F\20\210\211E\13F\202\21\221.\301H\60F\2G\20\210\211"
+  "'\233\324j\224\320(\64\213He\0H\20\207\211\65\234Ec\21QH\32\211\305F\0\0\0\0\4"
+  "\377\377\0";
+/*
   Fontname: open_iconic_www_1x
   Copyright: https://github.com/iconic/open-iconic, SIL OPEN FONT LICENSE
   Glyphs: 20/20
