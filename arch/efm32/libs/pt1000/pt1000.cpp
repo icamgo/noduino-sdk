a45f1d54d0bd191ad99619350c90f4e971e4463c
Jack Tan <jiankemeng@gmail.com>
2019-12-16 19:15:11 +0800

    arch/efm32/libs: add the pt1000

---
+++ arch/efm32/libs/pt1000/pt1000.cpp
@@ -0,0 +1,95 @@
+/*
+ *  Copyright (c) 2019 - 2029 MaiKe Labs
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+#include "pt1000.h"
+
+float cal_temp(uint32_t Rt)
+{
+	float temp;
+	float LR;
+	float HR;
+	float T;
+	uint8_t i;
+
+	uint8_t Bottom, Top;
+
+	if (Rt < PT100_TABLE[0]) {
+		return 0.0;
+	}
+
+	if (Rt > PT100_TABLE[99]) {
+		return 5000.0;
+	}
+
+	Bottom = 0;
+	Top = 99;
+
+	for (i = 49; (Top - Bottom) != 1;) {
+		if (Rt < PT100_TABLE[i]) {
+			Top = i;
+			i = (Top + Bottom) / 2;
+		} else if (Rt > PT100_TABLE[i]) {
+			Bottom = i;
+			i = (Top + Bottom) / 2;
+		} else {
+			T = i * 5.0 - 200.0;
+
+			return T;
+		}
+	}
+
+	T = i * 5.0 - 200.0;
+
+	LR = PT100_TABLE[Bottom];
+	HR = PT100_TABLE[Top];
+
+	temp = (((Rt - LR) * 5.0) / (HR - LR)) + T;
+
+	return temp;
+}
+
+float pt1000_get_temp()
+{
+	uint32_t rt = 0;
+	int a6 = 0, a7 = 0;
+
+	a6 = adc.read(A6);
+	a7 = adc.read(A7);
+
+#if 0
+	Serial.print("ADC6 = ");
+	Serial.println(a6);
+
+	Serial.print("ADC7 = ");
+	Serial.println(a7);
+#endif
+
+	// 1.1K x 10
+	rt = (uint32_t)(11000.0 / ((float)a6 / a7 - 1.0));
+
+#if 0
+	Serial.print("Rt = ");
+	Serial.println(rt);
+#endif
+
+	return cal_temp(rt);
+}
+
+void pt1000_init()
+{
+	adc.reference(adcRefVDD);
+}
