e2661c5d79f4a2b698236091f66c2f3cfa250365
Jack Tan <jiankemeng@gmail.com>
2019-12-19 15:23:45 +0800

    arch/efm32: update u8g2
    fixed the stupid incorrect parameter of SH1106

--- arch/efm32/libs/U8g2/src/clib/u8x8_d_ssd1306_128x32.c
+++ arch/efm32/libs/U8g2/src/clib/u8x8_d_ssd1306_128x32.c
@@ -268,7 +268,7 @@ static const u8x8_display_info_t u8x8_sh1106_128x32_noname_display_info =
   /* data_setup_time_ns = */ 40,
   /* write_pulse_width_ns = */ 150,	/* SSD1306: cycle time is 300ns, so use 300/2 = 150 */
   /* tile_width = */ 16,
-  /* tile_hight = */ 8,
+  /* tile_hight = */ 4,
   /* default_x_offset = */ 0,
   /* flipmode_x_offset = */ 0,
   /* pixel_width = */ 128,
