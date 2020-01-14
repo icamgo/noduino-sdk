bba84ec7c3065f9cdfa349044d73706c0bf658aa
Jack Tan <jiankemeng@gmail.com>
2020-01-14 16:01:28 +0800

    arch/efm32/libs: remove the sts add the sts3x

---
+++ arch/efm32/libs/sts3x/sts3x.cpp
@@ -0,0 +1,154 @@
+/*
+ *  Copyright (c) 2018 - 2028 MaiKe Labs
+ *
+ *  Library for STS3x digital Temperature sensor
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+#include "sts3x.h"
+#include "softi2c.h"
+
+#define STS3X_1MS			(F_CPU/1000)		/* 14MHz, 1Tick = 1/14us, 14000tick = 1000us */
+#define	sts3x_delay(x)		i2c_delay(x*STS3X_1MS)
+
+#define DATA_LEN			3
+
+static uint8_t crc8(const uint8_t *data, uint8_t len)
+{
+	// adapted from STS21 sample code from
+	// http://www.sensirion.com/en/products/humidity-temperature/download-center/
+
+	uint8_t crc = 0xff;
+	uint8_t i;
+	for (i = 0; i < len; ++i) {
+
+		crc ^= data[i];
+
+		for (uint8_t bit = 8; bit > 0; --bit) {
+			if (crc & 0x80) {
+				crc = (crc << 1) ^ 0x31;
+			} else {
+				crc = (crc << 1);
+			}
+		}
+	}
+	return crc;
+}
+
+uint8_t sts3x_clear_status()
+{
+	int ret;
+
+	//Request the user register
+	wire_beginTransmission(STS3X_ADDR_LOW);
+	wire_write(CLEAR_STATUS >> 8);
+	wire_write(CLEAR_STATUS & 0xff);
+	ret = wire_endTransmission();
+
+	// delay 11ms for 11bit resolution
+	sts3x_delay(11);
+	return ret;
+}
+
+uint8_t sts3x_reset(void)
+{
+	uint8_t ret;
+
+	wire_beginTransmission(STS3X_ADDR_LOW);
+	wire_write(SOFT_RESET >> 8);
+	wire_write(SOFT_RESET & 0xff);
+	ret = wire_endTransmission();
+
+	sts3x_delay(15);
+	return ret;
+}
+
+static uint16_t sts3x_read_sensor(uint16_t cmd)
+{
+	uint8_t data[DATA_LEN];
+	int ret;
+
+	wire_beginTransmission(STS3X_ADDR_LOW);
+	wire_write(cmd >> 8);
+	wire_write(cmd & 0xFF);
+	ret = wire_endTransmission();
+
+	// delay 11ms for 11bit resolution
+	sts3x_delay(11);
+
+	wire_requestFrom(STS3X_ADDR_LOW, 3);
+
+	sts3x_delay(1);
+
+	if (wire_available() < 3) {
+		INFO("SHT2x: wire request(read) timeout!");
+		return 1;
+	}
+
+	for (int i = 0; i < DATA_LEN; ++i) {
+		data[i] = wire_read();
+	}
+
+	if (crc8(data, 2) != data[2]) {
+		INFO("STS3X: crc error!");
+		return 2;
+	}
+
+#ifdef DEBUG
+	Serial.print(data[0], HEX);
+	Serial.print(" ");
+
+	Serial.print(data[1], HEX);
+	Serial.print(" ");
+
+	Serial.println(data[2], HEX);
+#endif
+
+	return ((data[0] << 8) | data[1]);
+}
+
+/*
+ * Gets the current temperature from the sensor.
+ * @return float - The temperature in Deg C
+ */
+float sts3x_get_temp(void)
+{
+	uint16_t sd = 2;
+	int cnt = 0;
+	float ret = -273.0;
+
+	//sts3x_reset();
+
+	while ((sd == 1 || sd == 2) && cnt <= 3) {
+		sd = sts3x_read_sensor(READ_HIGH_RES);
+		cnt++;
+	}
+
+	ret = (-45.0 + 175.0 * (float)sd / 65535.0);
+
+	if (ret < -40.0)
+		return -273.0;
+	else if (ret > 120.0)
+		return 300.0;
+	else
+		return ret;
+}
+
+uint8_t sts3x_init(uint8_t scl, uint8_t sda)
+{
+	wire_begin(scl, sda);
+
+	return 0;
+}
---
+++ arch/efm32/libs/sts3x/sts3x.h
@@ -0,0 +1,58 @@
+/*
+ *  Copyright (c) 2018 - 2028 MaiKe Labs
+ *
+ *  Library for STS3x digital temperature sensor
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+
+#ifndef __STS3X_H__
+#define __STS3X_H__
+
+#include <Arduino.h>
+
+//#define	DEBUG				1
+
+#ifdef DEBUG
+#define	INFO(x)				Serial.println(x)
+#else
+#define	INFO(x)
+#endif
+
+#define	STS3X_ADDR_LOW				0x4A
+#define	STS3X_ADDR_HIGH				0x4B
+
+#define CLEAR_STATUS				0x3041
+#define SOFT_RESET					0x30A2
+
+#define READ_HIGH_RES				0x2C06
+#define READ_MEDIUM_RES				0x2C0D
+#define READ_LOW_RES				0x2C0F
+
+#define R_HIGH_ALERT_SET			0xE11F
+#define R_HIGH_ALERT_CLR			0xE114
+#define R_LOW_ALERT_SET				0xE102
+#define R_LOW_ALERT_CLR				0xE109
+
+#define W_HIGH_ALERT_SET			0x611D
+#define W_HIGH_ALERT_CLR			0x6116
+#define W_LOW_ALERT_SET				0x610B
+#define W_LOW_ALERT_CLR				0x6100
+
+float sts3x_get_temp(void);
+
+uint8_t sts3x_reset(void);
+uint8_t sts3x_init(uint8_t scl, uint8_t sda);
+#endif
