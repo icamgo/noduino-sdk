24d310ae445973051e2097c4cb56cb23c6195fad
Jack Tan <jiankemeng@gmail.com>
2020-09-17 17:19:06 +0800

    sketch/aeco-ccx: update the crypto.h

--- sketch/aeco-ccx/crypto.h
+++ sketch/aeco-ccx/crypto.h
@@ -18,6 +18,10 @@ uint8_t ae33kk[] __attribute__ ((aligned(4))) = {
 	0x02, 0x01, 0x02, 0x03, 0x05, 0x07, 0x08, 0x09
 };
 
+uint8_t d_key[16] __attribute__((aligned(4)));
+
+uint8_t pld_outin[16] __attribute__((aligned(4)));
+
 /*
  * [IN]  p          Data p
  * [IN]  plen       Data p plen
@@ -28,17 +32,19 @@ void compute_mic(const uint8_t *p, uint16_t plen, const uint8_t *kk,
 					uint32_t *mic)
 {
 	blk_b0[5] = p[6];
-	blk_b0[6] = p[7]; 
+	blk_b0[6] = p[7];
 	blk_b0[7] = p[8];
 	blk_b0[8] = p[9];
 	blk_b0[9] = p[10];
 
-	blk_b0[10] = p[plen-8]; 
+	blk_b0[10] = p[plen-8];
 	blk_b0[11] = p[plen-7];
-	blk_b0[12] = p[plen-6]; 
-	blk_b0[13] = p[plen-5]; 
+	blk_b0[12] = p[plen-6];
+	blk_b0[13] = p[plen-5];
 
 	blk_b0[15] = plen & 0xFF;
+
+	memcpy(kk, p, 7);
 
 	AES_CBC128(aes_out, p+12, 16, kk, blk_b0, true);  /* true means encrypt. */
 
@@ -46,7 +52,7 @@ void compute_mic(const uint8_t *p, uint16_t plen, const uint8_t *kk,
 			(uint32_t) aes_out[1] << 8 | (uint32_t) aes_out[0]);
 }
 
-bool check_pkt_mic(uint8_t *p, int len)
+bool check_pkt_mic(const uint8_t *p, int len)
 {
 	uint32_t mic = 0;
 	uint8_t *p_mic = (uint8_t *)&mic;
@@ -89,29 +95,59 @@ bool set_pkt_mic(uint8_t *p, int len)
 	}
 }
 
-void payload_encrypt(const uint8_t *p, uint16_t plen, const uint8_t *kk,
-						uint8_t *aes_out)
+uint8_t *payload_encrypt(const uint8_t *p, uint16_t plen, const uint8_t *kk)
 {
-	memset(iv, 16, 0);
+	memset(iv, 0, 16);
+	iv[0] = 0x55;
 
-	iv[5] = p[6];
-	iv[6] = p[7]; 
-	iv[7] = p[8];
-	iv[8] = p[9];
-	iv[9] = p[10];
+	memcpy(iv+5, p, 5);
 
-	iv[10] = p[plen-8]; 
-	iv[11] = p[plen-7];
-	iv[12] = p[plen-6]; 
-	iv[13] = p[plen-5]; 
+	iv[10] = 0x6;
+	iv[11] = 0x4;
+	iv[12] = 0x2;
+	iv[13] = 0x2;
 
 	iv[15] = plen & 0xFF;
 
-	AES_CBC128(aes_out, p, plen, kk, iv, true);  /* true means encrypt. */
+	memcpy(kk, p, 7);
+
+	memset(pld_outin, 0, 16);
+	memcpy(pld_outin, p+7, 8);
+	memcpy(pld_outin+8, p+16, 8);
+
+	AES_CBC128(pld_outin, pld_outin, 16, kk, iv, true);
+
+	memcpy(p+7, pld_outin, 8);
+	memcpy(p+16, pld_outin+8, 8);
+
+	return (uint8_t *)pld_outin;
 }
 
-void payload_decrypt(const uint8_t *p, uint16_t plen, const uint8_t *kk,
-						uint8_t *aes_out)
+uint8_t *payload_decrypt(const uint8_t *p, uint16_t plen, const uint8_t *kk)
 {
+	memset(iv, 0, 16);
+	iv[0] = 0x55;
 
+	memcpy(iv+5, p, 5);
+
+	iv[10] = 0x6;
+	iv[11] = 0x4;
+	iv[12] = 0x2;
+	iv[13] = 0x2;
+
+	iv[15] = plen & 0xFF;
+
+	memcpy(kk, p, 7);
+
+	memset(pld_outin, 0, 16);
+	memcpy(pld_outin, p+7, 8);
+	memcpy(pld_outin+8, p+16, 8);
+
+	AES_DecryptKey128(d_key, kk);
+	AES_CBC128(pld_outin, pld_outin, 16, d_key, iv, false);
+
+	memcpy(p+7, pld_outin, 8);
+	memcpy(p+16, pld_outin+8, 8);
+
+	return (uint8_t *)pld_outin;
 }
