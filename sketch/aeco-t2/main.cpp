37b45abc39436f306958888abe8f25feca157ba9
Jack Tan <jiankemeng@gmail.com>
2021-03-19 16:03:36 +0800

    sketch/aeco-t2: update to support new proto

--- sketch/aeco-t2/main.cpp
+++ sketch/aeco-t2/main.cpp
@@ -16,36 +16,69 @@
  *
 */
 
-#include "softspi.h"
-#include "sx1272.h"
 #include "pt1000.h"
 
 #include "rtcdriver.h"
 #include "math.h"
 #include "em_wdog.h"
 
-
 //#define	DEBUG					1
+
+#define FW_VER						"Ver 1.2"
+
+//#define CONFIG_2MIN					1
+
+#define ENABLE_CRYPTO				1
+
+#define ENABLE_CAD					1
+
+#define ENABLE_RF					1
+
+#define ENABLE_T_TEST				1
+
+#ifdef ENABLE_T_TEST
+#define DELTA_T						1.5
+static uint32_t cnt_01 = 0;
+#else
+#define DELTA_T						1.0
+#endif
+
+
+#define	PAYLOAD_LEN					30		/* 30+2+4 = 36B */
+
+#ifdef ENABLE_RF
+#include "softspi.h"
+#include "sx1272.h"
+#endif
+
+#ifdef ENABLE_CRYPTO
+#include "crypto.h"
+#endif
 
 /* Timer used for bringing the system back to EM0. */
 RTCDRV_TimerID_t xTimerForWakeUp;
 
-static uint32_t sample_period = 20;		/* 20s */
-
+#ifndef CONFIG_2MIN
+static uint32_t sample_period = 18;			/* 20s */
 static uint32_t sample_count = 0;
-#define		HEARTBEAT_TIME			7200
-
+#define		HEARTBEAT_TIME			6600	/* 120min */
 static float old_temp = 0.0;
-static float cur_temp = 0.0;
+#else
+//static uint32_t sample_period = 110;		/* 120s */
+static uint32_t sample_period = 18;			/* 20s */
+#endif
 
-//#define	TX_TESTING				1
+static float cur_temp = 0.0;
+static float cur_vbat = 0.0;
+
+#ifdef MONITOR_CURRENT
+static float cur_curr = 0.0;
+#endif
+
+static uint32_t need_push = 0;
 
 #define	PWR_CTRL_PIN			8		/* PIN17_PC14_D8 */
 #define	KEY_PIN					0		/* PIN01_PA00_D0 */
-
-static uint8_t need_push = 0;
-
-#define ENABLE_CAD				1
 
 #define	TX_TIME					1800		// 1800ms
 #define DEST_ADDR				1
@@ -58,6 +91,8 @@ static uint8_t need_push = 0;
 #define	DELTA_TX			1
 #define	TIMER_TX			2
 #define	KEY_TX				3
+#define	EL_TX				4
+#define	WL_TX				5
 
 #else
 #define node_addr				110
@@ -68,39 +103,26 @@ static uint8_t need_push = 0;
 
 #define MAX_DBM					20
 
-
-//#define WITH_ACK
+uint8_t message[PAYLOAD_LEN+6] __attribute__((aligned(4))) = { 0x47, 0x4F, 0x33 };
 
 #ifdef CONFIG_V0
-uint8_t message[32] = { 0x47, 0x4F, 0x33 };
-uint8_t tx_cause = RESET_TX;
-uint16_t tx_count = 0;
-#else
-uint8_t message[32];
+uint32_t tx_cause = RESET_TX;
+uint16_t tx_count __attribute__((aligned(4))) = 0;
+uint32_t tx_ok_cnt = 0;
 #endif
 
 #ifdef DEBUG
-
 #define INFO_S(param)			Serial.print(F(param))
 #define INFO_HEX(param)			Serial.print(param,HEX)
 #define INFO(param)				Serial.print(param)
 #define INFOLN(param)			Serial.println(param)
-#define FLUSHOUTPUT					Serial.flush();
-
+#define FLUSHOUTPUT				Serial.flush();
 #else
-
 #define INFO_S(param)
 #define INFO(param)
 #define INFOLN(param)
 #define FLUSHOUTPUT
-
 #endif
-
-#ifdef WITH_ACK
-#define	NB_RETRIES			2
-#endif
-
-void push_data();
 
 char *ftoa(char *a, double f, int precision)
 {
@@ -121,6 +143,8 @@ char *ftoa(char *a, double f, int precision)
 	return ret;
 }
 
+void push_data(bool cad);
+
 void power_on_dev()
 {
 	digitalWrite(PWR_CTRL_PIN, HIGH);
@@ -129,6 +153,50 @@ void power_on_dev()
 void power_off_dev()
 {
 	digitalWrite(PWR_CTRL_PIN, LOW);
+}
+
+float fetch_mcu_temp()
+{
+	float temp = 0.0;
+	for(int i=0; i<3; i++) {
+		temp += adc.temperatureCelsius();
+	}
+	temp /= 3.0;
+
+	return temp;
+}
+
+#ifdef MONITOR_CURRENT
+float fetch_current()
+{
+	adc.reference(adcRef1V25);
+
+	int ad = 0;
+
+	for (int i = 0; i < 5; i++) {
+		ad += adc.read(A6, A7);
+	}
+
+	cur_curr = 1250.0*ad/2.0/2048.0/0.7 / 5;
+
+	INFO("ADC differential ch6 ch7 read:");
+	INFOLN(ad);
+
+	INFO("The consumption current (mA): ");
+
+	return cur_curr;
+}
+#endif
+
+float fetch_vbat()
+{
+	float vbat = 0;
+
+	for (int i = 0; i < 5; i++) {
+		vbat += adc.readVbat();
+	}
+
+	return vbat/5.0;
 }
 
 void check_sensor(RTCDRV_TimerID_t id, void *user)
@@ -140,6 +208,9 @@ void check_sensor(RTCDRV_TimerID_t id, void *user)
 
 	RTCDRV_StopTimer(xTimerForWakeUp);
 
+#ifdef ENABLE_RF
+
+#ifndef CONFIG_2MIN
 	sample_count++;
 
 	if (sample_count >= HEARTBEAT_TIME/20) {
@@ -147,8 +218,9 @@ void check_sensor(RTCDRV_TimerID_t id, void *user)
 		tx_cause = TIMER_TX;
 		sample_count = 0;
 	}
+#endif
 
-	power_on_dev();		// turn on device power
+	power_on_dev();	// turn on device power
 
 	pt1000_init();	// initialization of the sensor
 
@@ -156,50 +228,79 @@ void check_sensor(RTCDRV_TimerID_t id, void *user)
 
 	power_off_dev();
 
-#ifdef TX_TESTING
-	need_push = 0x5a;
-	tx_cause = TIMER_TX;
-#else
-	if (fabsf(cur_temp - old_temp) > 1.0) {
+#ifndef CONFIG_2MIN
+	float dp = fabsf(cur_temp - old_temp);
+
+	if (dp >= DELTA_T) {
 
 		need_push = 0x5a;
-#ifdef CONFIG_V0
 		tx_cause = DELTA_TX;
-#endif
+
+	#ifdef ENABLE_T_TEST
+		cnt_01 = 0;
+	#endif
+
+		return;
 	}
+
+	#ifdef ENABLE_T_TEST
+	if (dp >= DELTA_T/2 && dp < DELTA_T) {
+
+		cnt_01++;
+
+		if (cnt_01 >= 3) {
+			need_push = 0x5a;
+			tx_cause = DELTA_TX;
+
+			cnt_01 = 0;
+		}
+
+	} else if (dp < DELTA_T/2) {
+
+		cnt_01 = 0;
+	}
+	#endif
+#else
+	// 2min fixed interval
+	need_push = 0x5a;
+	tx_cause = TIMER_TX;
+#endif
+
 #endif
 }
 
 void trig_check_sensor()
 {
-	noInterrupts();
-
 	need_push = 0x5a;
 #ifdef CONFIG_V0
 	tx_cause = KEY_TX;
 #endif
-
-	interrupts();
 }
 
 void setup()
 {
 	Ecode_t e;
 
-#if 0
-	/* Initialize EM23 with default parameters */
-	EMU_EM23Init_TypeDef em23Init = EMU_EM23INIT_DEFAULT;
-	EMU_EM23Init(&em23Init);
-#endif
-
 	WDOG_Init_TypeDef wInit = WDOG_INIT_DEFAULT;
 
 	/* Watchdog setup - Use defaults, excepts for these : */
 	wInit.em2Run = true;
 	wInit.em3Run = true;
-	wInit.perSel = wdogPeriod_256k;	/* 256k 1kHz periods should give 256 seconds */
 
-	// dev power ctrl
+#ifdef ENABLE_CRYPTO
+	crypto_init();
+#endif
+
+#ifdef CONFIG_2MIN
+	//wInit.perSel = wdogPeriod_128k;	/* 128k 1kHz periods should give 128 seconds */
+	wInit.perSel = wdogPeriod_32k;	/* 32k 1kHz periods should give 32 seconds */
+#else
+	wInit.perSel = wdogPeriod_32k;	/* 32k 1kHz periods should give 32 seconds */
+#endif
+
+	cur_vbat = fetch_vbat();
+
+	// init dev power ctrl pin
 	pinMode(PWR_CTRL_PIN, OUTPUT);
 
 	power_off_dev();
@@ -224,10 +325,9 @@ void setup()
 	need_push = 0x5a;
 }
 
+#ifdef ENABLE_RF
 void qsetup()
 {
-	power_on_dev();		// turn on device power
-
 #ifdef CONFIG_V0
 	sx1272.setup_v0(TXRX_CH, MAX_DBM);
 #else
@@ -238,23 +338,17 @@ void qsetup()
 #ifdef ENABLE_CAD
 	sx1272._enableCarrierSense = true;
 #endif
-
 }
+#endif
 
 #ifdef CONFIG_V0
 uint64_t get_devid()
 {
-#if 1
 	uint64_t *p;
 
 	p = (uint64_t *)0x0FE00008;
 
 	return *p;
-#else
-	return 11903500001ULL;
-#endif
-
-	//return 11907480002ULL;	// T2p
 }
 
 uint16_t get_crc(uint8_t *pp, int len)
@@ -269,28 +363,48 @@ uint16_t get_crc(uint8_t *pp, int len)
 }
 #endif
 
-void push_data()
+#ifdef ENABLE_RF
+void push_data(bool cad_on)
 {
-	long startSend;
-	long endSend;
-
-	float vbat = 0.0;
-
-	uint8_t r_size;
+	long start;
+	long end;
 
 	int e;
 
-	qsetup();
+#ifdef CONFIG_V0
+	uint8_t *pkt = message;
+
+	memset(pkt, 0, PAYLOAD_LEN+6);
+	pkt[0] = 0x47; pkt[1] = 0x4F; pkt[2] = 0x33;
+#endif
+
+	WDOG_Feed();
+
+	////////////////////////////////
+#ifdef MONITOR_CURRENT
+	cur_curr = fetch_current();
+
+	noInterrupts();
+
+	if (cur_curr > 1.9)
+		pkt[15] = EL_TX;
+	else
+		pkt[15] = tx_cause;
+
+	interrupts();
+#else
+	pkt[15] = tx_cause;
+#endif
+	////////////////////////////////
+
+	power_on_dev();		// turn on device power
 
 	if (KEY_TX == tx_cause || RESET_TX == tx_cause) {
 		pt1000_init();
-		cur_temp = pt1000_get_temp();		// 'C
+		cur_temp = pt1000_get_temp();
 	}
 
-	vbat = adc.readVbat();
-
 #ifdef CONFIG_V0
-	uint8_t *pkt = message;
 	uint64_t devid = get_devid();
 
 	uint8_t *p = (uint8_t *) &devid;
@@ -306,120 +420,124 @@ void push_data()
 
 	pkt[11] = p[1]; pkt[12] = p[0];
 
-	ui16 = vbat * 1000;
+	cur_vbat = fetch_vbat();
+	ui16 = cur_vbat * 1000;
 	pkt[13] = p[1]; pkt[14] = p[0];
 
-	pkt[15] = tx_cause;
+	float chip_temp = fetch_mcu_temp();
+
+	// Humidity Sensor data	or Water Leak Sensor data
+	pkt[20] = 0;
+
+	// Internal Temperature of the chip
+	pkt[21] = (int8_t)roundf(chip_temp);
+
+	// Internal humidity to detect water leak of the shell
+	pkt[22] = 255;
+
+	// Internal current consumption
+#ifdef MONITOR_CURRENT
+	pkt[23] = (int8_t)roundf(cur_curr);
+#else
+	pkt[23] = 0;
+#endif
 
 	p = (uint8_t *) &tx_count;
-	pkt[16] = p[1]; pkt[17] = p[0];
+	pkt[PAYLOAD_LEN-2] = p[1]; pkt[PAYLOAD_LEN-1] = p[0];
 	tx_count++;
 
-	ui16 = get_crc(pkt, 18);
+	/////////////////////////////////////////////////////////
+	/*
+	 * 2. crc
+	 * 3. set mic
+	*/
+	ui16 = get_crc(pkt, PAYLOAD_LEN);
 	p = (uint8_t *) &ui16;
-	pkt[18] = p[1]; pkt[19] = p[0];
 
-#else
-	char vbat_s[10], pres_s[10];
-	ftoa(vbat_s, vbat, 2);
-	ftoa(pres_s, cur_temp, 2);
+	pkt[PAYLOAD_LEN] = p[1]; pkt[PAYLOAD_LEN+1] = p[0];
 
-	r_size = sprintf((char *)message, "\\!U/%s/P/%s", vbat_s, pres_s);
-
-	INFO("Sending ");
-	INFOLN((char *)message);
-
-	INFO("Real payload size is ");
-	INFOLN(r_size);
+#ifdef ENABLE_CRYPTO
+	set_pkt_mic(pkt, PAYLOAD_LEN+6);
 #endif
+	/////////////////////////////////////////////////////////
+#else
+	// not CONFIG_V0
+
+#endif
+
+	qsetup();
 
 #ifdef ENABLE_CAD
-	sx1272.CarrierSense();
+	if (cad_on) {
+		sx1272._enableCarrierSense = true;
+		sx1272.CarrierSense();
+	} else {
+		sx1272._enableCarrierSense = false;
+	}
 #endif
 
-	startSend = millis();
+#ifdef DEBUG
+	start = millis();
+#endif
 
 #ifdef CONFIG_V0
-	e = sx1272.sendPacketTimeout(DEST_ADDR, message, 24, TX_TIME);
+	if (cad_on) {
+		e = sx1272.sendPacketTimeout(DEST_ADDR, message, PAYLOAD_LEN+6, TX_TIME);
+	} else {
+		e = sx1272.sendPacketTimeout(DEST_ADDR, message, PAYLOAD_LEN+6, 300);
+	}
 #else
-	// just a simple data packet
-	sx1272.setPacketType(PKT_TYPE_DATA);
 
-	// Send message to the gateway and print the result
-	// with the app key if this feature is enabled
-#ifdef WITH_ACK
-	int n_retry = NB_RETRIES;
-
-	do {
-		e = sx1272.sendPacketTimeoutACK(DEST_ADDR,
-						message, r_size);
-
-		if (e == 3)
-			INFO("No ACK");
-
-		n_retry--;
-
-		if (n_retry)
-			INFO("Retry");
-		else
-			INFO("Abort");
-
-	} while (e && n_retry);
-#else
-	// 10ms max tx time
-	e = sx1272.sendPacketTimeout(DEST_ADDR, message, r_size, TX_TIME);
-#endif
 #endif
 
 	if (!e) {
 		// send message succesful, update the old_temp
+	#ifndef CONFIG_2MIN
 		old_temp = cur_temp;
+	#endif
+		tx_ok_cnt++;
 	}
 
-	endSend = millis();
-
-	INFO("LoRa pkt size ");
-	INFOLN(r_size);
+#ifdef DEBUG
+	end = millis();
 
 	INFO("LoRa Sent in ");
-	INFOLN(endSend - startSend);
+	INFOLN(end - start);
 
 	INFO("LoRa Sent w/CAD in ");
-	INFOLN(endSend - sx1272._startDoCad);
+	INFOLN(end - sx1272._startDoCad);
 
 	INFO("Packet sent, state ");
 	INFOLN(e);
+#endif
 
-	e = sx1272.setSleepMode();
-	if (!e)
-		INFO("Successfully switch into sleep mode");
-	else
-		INFO("Could not switch into sleep mode");
+	if (cad_on) {
+		sx1272.setSleepMode();
+		digitalWrite(SX1272_RST, LOW);
 
-	digitalWrite(SX1272_RST, LOW);
+		spi_end();
 
-	spi_end();
-
-	power_off_dev();
+		// dev power off
+		power_off_dev();
+	}
 }
+#endif
 
 void loop()
 {
-	INFO("Clock Freq = ");
-	INFOLN(CMU_ClockFreqGet(cmuClock_CORE));
-
-	//INFOLN("Feed the watchdog");
-
+#ifdef ENABLE_RF
 	if (0x5a == need_push) {
-		push_data();
-
+		push_data(true);
 		need_push = 0;
 	}
+#endif
 
 	power_off_dev();
-	digitalWrite(SX1272_RST, LOW);
 
+#ifdef ENABLE_RF
+	digitalWrite(SX1272_RST, LOW);
 	spi_end();
+#endif
 
 	/*
 	 * Enable rtc timer before enter deep sleep
