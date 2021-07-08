a353ca17dd0c7f4567399ab78c37b1865343eaae
Jack Tan <jiankemeng@gmail.com>
2021-07-08 19:28:14 +0800

    efm32/core: enable the rx int of usart

--- arch/efm32/cores/efm32/HardwareSerial.cpp
+++ arch/efm32/cores/efm32/HardwareSerial.cpp
@@ -331,10 +331,8 @@ void HardwareSerial::begin(const uint32_t baud,uint8_t config){
 #endif
     USART_InitAsync(this->instance, &initasync);
     this->instance->IFC = _USART_IFC_MASK;
-	#ifndef UART_NO_RX_INT
-	Serial.println(__LINE__);
+	#ifndef UART_NO_RX_INTx
 	this->instance->IEN = USART_IEN_RXDATAV;			/* RX Data Valid Interrupt Enable */
-	Serial.println(__LINE__);
     this->instance->ROUTE |=  USART_ROUTE_TXPEN | USART_ROUTE_RXPEN | USART_ROUTE_LOCATION_LOCx;
 	#else
     this->instance->ROUTE |=  USART_ROUTE_TXPEN | USART_ROUTE_LOCATION_LOCx;
